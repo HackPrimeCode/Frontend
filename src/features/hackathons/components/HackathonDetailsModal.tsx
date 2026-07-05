c0132216096d67f957dbfc522f19986a67a8a7e4
Arteluff <mastertema2005@gmail.com>
2026-07-05 10:02:23 +0300

    feat: remove console.count

--- src/features/hackathons/components/HackathonDetailsModal.tsx
+++ src/features/hackathons/components/HackathonDetailsModal.tsx
@@ -72,7 +72,6 @@ export default function HackathonDetailsModal({
 }: HackathonDetailsModalProps) {
   const authContext = useSelector(selectCurrentContext);
   const isParticipant = authContext?.teamId !== null;
-  console.count("HackathonDetailsModal render");
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
